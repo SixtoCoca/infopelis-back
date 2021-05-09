1fdbc48b6fbd5e33dd55fb85baf9d3c0f1b51e67
SixtoCoca <sixtococacruz@gmail.com>
2021-05-09 21:03:59 +0200

    añadido web scrapping

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cliente from "./routes/cliente";
 import omdbapi from "./routes/omdbapi";
 import moviedb from "./routes/moviedb";
+import scrap from "./routes/scrap";
 
 //Creando el enrutador
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use("/cliente", cliente);
 router.use("/omdb", omdbapi);
 router.use("/moviedb", moviedb);
+router.use("/scrap", scrap);
 
 export default router;
---
+++ src/services/scrap.js
@@ -0,0 +1,27 @@
+import cheerio from "cheerio";
+import request from "request";
+
+async function info(id) {
+  var valor = "";
+  if(id){
+    valor = id;
+  }else{
+    valor = "tt0076759"
+  }
+  var url = "https://www.imdb.com/title/"+ valor +"/?ref_=rvi_tt";
+  request(url, (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+      const $=cheerio.load(body);
+      var titulo = "";
+      titulo = $('.plot_summary');
+      console.log(titulo.children().first().text())
+      return titulo;
+    }
+  });
+}
+
+const service = {
+  info,
+};
+
+export default service;
